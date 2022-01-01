579dc9b70df2bbab4d476cf277bdd1af89881ac9
SarthakKaner <54986194+SarthakKaner@users.noreply.github.com>
2022-01-01 12:29:37 +0530

    Create 11. 169 - Majority Element E.java

---
+++ 11./11. 169 - Majority Element E.java
@@ -0,0 +1,33 @@
+// Moore's voting algorithm
+
+class Solution {
+    public int majorityElement(int[] nums) {
+        
+        int count = 0,ele = 0;
+        
+        for(int i = 0; i < nums.length;i++){
+            if(count == 0){
+                ele = nums[i];
+            }
+            if(ele == nums[i]){
+                count++;
+            }else{
+                count--;
+            }
+        }
+        return ele;
+    }
+}
+
+// using HashMap
+
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i=0;i < nums.length;i++){
+            if(map.containsKey(nums[i])){
+                map.put(nums[i],map.get(nums[i])+1);
+            }else{
+                map.put(nums[i],1);
+            }
+        }
+        Integer key = Collections.max(map.entrySet(), Map.Entry.comparingByValue()).getKey();
+        return key;
