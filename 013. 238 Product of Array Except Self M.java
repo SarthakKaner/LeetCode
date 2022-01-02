38d0907aea1de9d99839db1f528bd22f292c5269
SarthakKaner <54986194+SarthakKaner@users.noreply.github.com>
2022-01-02 11:29:59 +0530

    Update 013. 238 Product of Array Except Self M.java

--- 013. 238 Product of Array Except Self M.java
+++ 013. 238 Product of Array Except Self M.java
@@ -1,4 +1,30 @@
+// O(1) space
+
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        
+        int[] result = new int[nums.length];
+        result[0] = 1;
+        
+        for(int i=1; i < nums.length; i++){
+            result[i] = nums[i - 1] * result[i - 1]; 
+        }
+        
+        int right = 1;
+        
+        for(int i=nums.length - 1; i >= 0; i--){
+            result[i] = result[i]  *  right; 
+            right *= nums[i];     
+        }
+
+        return result;
+        
+    }
+}
+
 // Optimal
+
+/*
 
 class Solution {
     public int[] productExceptSelf(int[] nums) {
@@ -31,6 +57,8 @@ class Solution {
         
     }
 }
+
+*/
 
 // Brute force - TLE
     // for(int i=0; i < nums.length; i++){
