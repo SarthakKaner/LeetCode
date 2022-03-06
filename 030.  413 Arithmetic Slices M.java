bd35e46d301ed75664638504a235c4d241c81999
SarthakKaner <54986194+SarthakKaner@users.noreply.github.com>
2022-03-06 21:42:30 +0530

    Update 030.  413 Arithmetic Slices M.java

--- 030.  413 Arithmetic Slices M.java
+++ 030.  413 Arithmetic Slices M.java
@@ -1,26 +1,23 @@
 class Solution {
     public int numberOfArithmeticSlices(int[] nums) {
         
-        if(nums.length < 3)
-            return 0;
-        
-        int count = 0;
         int[] arr = new int[nums.length];
-        for(int i=0;i<nums.length;i++){
-            arr[i] = 0;
+        int sum = 0;
+        
+        if(nums.length < 3){
+            return 0;
         }
         
-        
-        for(int i=0;i<=nums.length - 3;i++){
-            if(nums[i+1] - nums[i] == nums[i+2] - nums[i+1]){
-                arr[i+2] = arr[i+1] + 1;
+        for(int i=2;i<nums.length;i++){
+            if(nums[i] - nums[i-1] == nums[i-1] - nums[i-2]){
+                arr[i] = arr[i-1] + 1;
             }
         }
         
-        for(int i=0;i<nums.length;i++){
-            count += arr[i] ;
+        for(int i=0;i < arr.length;i++){
+            sum += arr[i];
         }
         
-        return count;
+        return sum;
     }
 }
